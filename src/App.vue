3071d603570aa9db0a43204b90bdef1c97f7647e
Dmitry <rinini9009@gmail.com>
2021-08-25 14:49:11 +0300

    upd app emits

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,7 @@
 import TheNavbar from './components/TheNavbar'
 import {useStore} from "vuex";
 import {onMounted} from "vue";
-import objToArr from "./utils/objToArr";
-
-import {getDatabase, ref, onValue, onDisconnect,  set,increment} from "firebase/database";
-
+import fireBaseInitEmits from './utils/f.b.initEmits'
 
 export default {
   components: {
@@ -21,47 +18,9 @@ export default {
   },
   setup() {
     const store = useStore()
-    const db = getDatabase();
-    const presenceRef = ref(db, "peopleConnected");
 
     onMounted(async () => {
-      const connectedRef = ref(db, ".info/connected");
-
-      // onValue(connectedRef, async (snap) => {
-      //   if (snap.val() === true) {
-      //     const res = await get(ref(db,'peopleConnected'))
-      //       set(ref(db, 'peopleConnected'),+res.val() + 1)
-      //       // console.log("connected");
-      //   } else {
-      //     // console.log("not connected");
-      //   }
-      // });
-
-      onValue(connectedRef, async (snap) => {
-        if (snap.val() === true) {
-            set(ref(db, 'peopleConnected'),increment(+1))
-        }
-      });
-
-      onDisconnect(presenceRef).set(increment(-1))
-
-      onValue(ref(db, 'peopleConnected'), (snapshot) => {
-        const res = snapshot.val();
-        if (res) {
-          store.commit('setOnlinePeople', +res)
-        }
-      });
-
-
-      onValue(ref(db, 'tasks'), (snapshot) => {
-        const res = snapshot.val();
-        if (res) {
-          store.commit('setTaskArr', objToArr(res).sort((a, b) => b.timeStamp - a.timeStamp))
-        } else {
-          store.commit('setTaskArr', [])
-        }
-      });
-
+      fireBaseInitEmits(store)
     })
     return {}
   }
--- src/utils/f.b.initEmits.js
+++ src/utils/f.b.initEmits.js
@@ -1,24 +1,11 @@
-import {initializeApp} from 'firebase/app';
-
 import {getDatabase, increment, onDisconnect, onValue, ref, set} from "firebase/database";
 import objToArr from "./objToArr";
 
-const db = getDatabase();
-const connectedRef = ref(db, ".info/connected");
-const presenceRef = ref(db, "peopleConnected");
-
 
 export default function (store){
-  initializeApp({
-    apiKey: "AIzaSyDwGS27BbfHEolJDUPRgdpHPYiqtwvFIgs",
-    authDomain: "vue-with-http-822a4.firebaseapp.com",
-    databaseURL: "https://vue-with-http-822a4-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "vue-with-http-822a4",
-    storageBucket: "vue-with-http-822a4.appspot.com",
-    messagingSenderId: "515574911494",
-    appId: "1:515574911494:web:fd2dce2e1eeacea11b6987"
-  })
-
+  const db = getDatabase();
+  const connectedRef = ref(db, ".info/connected");
+  const presenceRef = ref(db, "peopleConnected");
 
   onValue(connectedRef, async (snap) => {
     if (snap.val() === true) {
@@ -26,15 +13,12 @@ export default function (store){
     }
   });
 
-  onDisconnect(presenceRef).set(increment(-1))
-
   onValue(ref(db, 'peopleConnected'), (snapshot) => {
     const res = snapshot.val();
     if (res) {
       store.commit('setOnlinePeople', +res)
     }
   });
-
 
   onValue(ref(db, 'tasks'), (snapshot) => {
     const res = snapshot.val();
@@ -44,5 +28,8 @@ export default function (store){
       store.commit('setTaskArr', [])
     }
   });
+
+  onDisconnect(presenceRef).set(increment(-1))
+
 }
 
